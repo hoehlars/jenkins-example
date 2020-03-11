2786adb42855d29f4d630322bdc95d03f0b9bd43
Lars Höhener <hoehelar@students.zhaw.ch>
2020-03-11 13:54:58 +0100

    Initial commit.

---
+++ src/test/java/TestExample.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestExample {
+
+    @Test
+    public void testOneEqualsOne() {
+        assertEquals(1,1);
+    }
+}
